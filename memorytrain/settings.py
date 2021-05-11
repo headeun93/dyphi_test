adb9dc0c0b4e2e1dc7d41415de1d7cf148389c7f
headeun93 <55781686+headeun93@users.noreply.github.com>
2021-05-11 15:20:56 +0900

    Update settings.py

--- memorytrain/settings.py
+++ memorytrain/settings.py
@@ -112,7 +112,7 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS = [0.
+AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
